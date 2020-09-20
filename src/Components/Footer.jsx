8916b859053ddf10bcf7617a932ac8dbb59981a2
abdallah <abab231196@gmail.com>
2020-09-20 11:15:08 +0200

    first commit

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+  const year = new Date().getFullYear();
+  console.log(year);
+
+  const y = new Date();
+  console.log(y);
+
+  return (
+    <footer>
+      <p id='name'> copyright { year } </p>
+    </footer>
+  );
+}
+
+export default Footer;
